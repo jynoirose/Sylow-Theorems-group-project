1f61826dd6f2e49f8e383139f80e8594baab4612
jynoirose <chen.jiaying200211@gmail.com>
2025-10-29 01:07:53 +0000

    delete the space of file name and test the mathlib

--- SylowTheromsProject/SylowTheromsProject/Basic.lean
+++ SylowTheromsProject/SylowTheromsProject/Basic.lean
@@ -1,1 +1,8 @@
-def hello := "world"
+-- def hello := "world"
+
+import Mathlib.GroupTheory.Sylow --test
+import Mathlib.Data.Nat.Prime.Basic  --test
+
+#check Nat.Prime --test
+#check Sylow --test
+#eval 2 + 3 --test
